d7ca8e4ecadbe1deeec578dc382c0f13ae2351e6
unknown <abu040810@gmail.com>
2025-03-07 16:43:17 +0500

    arrow

--- script.js
+++ script.js
@@ -65,3 +65,14 @@ const CalculateXojiaka=function(a,b)
 console.log(CalculateXojiaka(9,7));
 
 // function expression
+
+
+// arrow function =>
+
+const CalculateXojiaka1=(a,b)=>
+{
+    return a+b;
+}
+
+// amal 1ta bolsa => bir qatorda blok sxemasiz yoziladi
+console.log(CalculateXojiaka1(9,8));
